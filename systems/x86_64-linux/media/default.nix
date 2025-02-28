1577244e62840bd4de646218923a3c266066727f
Pumita <hi+git@hackr.es>
2025-02-28 18:39:35 +0100

    🪼 Jellyfin (Cris)

---
+++ systems/x86_64-linux/media/default.nix
@@ -0,0 +1,64 @@
+{ namespace, ... }: {
+  "${namespace}" = {
+    # Hardware
+    hardware.disk = "/dev/sda";
+    hardware.isVM = true;
+
+    # i18n
+    i18n.timezone = "Europe/Madrid";
+
+    # Networking
+    networking.hostName = "media";
+    networking.ipv4 = "192.168.1.3";
+    networking.firewall.enable = true;
+
+    # System
+    nix.gc.enable = true;
+    nixos.followFlake = "github:cosasdepuma/nix";
+
+    # Users
+    users.administrator.username = "elliot";
+    users.administrator.description = "Hello, friend.";
+    users.administrator.sshPubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP9RzisL6wVQK3scDyEPEpFgrcdFYkW9LssnWlORGXof nixos@infra";
+
+    # ---- Services
+
+    # Fail2Ban
+    services.fail2ban.enable = true;
+
+    # Podman
+    services.podman.enable = true;
+
+    # SSHd
+    services.sshd.enable = true;
+    services.sshd.port = 10022;
+  };
+
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
+  environment.systemPackages = [
+    pkgs.jellyfin
+    pkgs.jellyfin-web
+    pkgs.jellyfin-ffmpeg
+  ];
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver
+      vaapiVdpau
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      # OpenCL support for intel CPUs before 12th gen
+      # see: https://github.com/NixOS/nixpkgs/issues/356535
+      intel-compute-runtime-legacy1 
+      vpl-gpu-rt # QSV on 11th gen or newer
+      intel-media-sdk # QSV up to 11th gen
+    ];
+  };
+
+}
