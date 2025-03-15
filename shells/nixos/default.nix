4d08212ae4e4b3075355c0cd918b49625cdfabd0
Pumita <hi+git@hackr.es>
2025-03-15 03:43:19 +0100

    💻 Development environmet (without linter and with nix-darwin)

--- shells/nixos/default.nix
+++ shells/nixos/default.nix
@@ -1,4 +1,5 @@
 { pkgs }: with pkgs; mkShell {
   NIX_CONFIG = "experimental-features = nix-command flakes";
   buildInputs = [ nh nixos-anywhere nixos-rebuild ];
+  shellHook = "alias darwin-rebuild='nix run nix-darwin#darwin-rebuild --'";
 }
